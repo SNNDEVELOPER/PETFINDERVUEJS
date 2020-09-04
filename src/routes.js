79063592602b784aafa6103435840c22ce5a0829
Shaun Nelson <shaun@shaunnelson.com>
2020-09-04 13:19:28 -0400

    Initial Commit
    Initial Commit

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import petfinderPets from './components/petfinderPets.vue';
+import petfinderDetails from './components/petfinderDetails.vue';
+
+export const routes = [
+    { path: '*', component: petfinderPets},
+    {  name: 'pets', path: '/pets', component: petfinderPets },
+    { name: 'petdetails', path: '/petdetails', component: petfinderDetails }
+
+];
